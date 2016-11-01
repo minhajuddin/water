deb72c73fe850bd4b8ab6448c83774753e838930
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-11-01 11:19:03 +0530

    Added the builder strucdt

---
+++ lib/water/builder.ex
@@ -0,0 +1,24 @@
+defmodule Water.Builder do
+  @moduledoc """
+  Once you `use` this module you get a method named `new` defined on your module
+
+  #### Example
+
+      defmodule User do
+        defstruct [:name, :email, :dob]
+        use Water.Builder
+      end
+
+      User.new(name: "Mujju", dob: "Jan 01") # => %User{name: "Mujju", email: nil, dob: "Jan 01"}
+
+  This gives you a simple method which accepts a keyword list and returns an
+  instance of the current struct
+  """
+  defmacro __using__(_opts) do
+    quote do
+      def new(params) do
+        struct(__MODULE__, params)
+      end
+    end
+  end
+end
